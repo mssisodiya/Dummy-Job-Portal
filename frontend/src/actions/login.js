24c4cf648835ec52078e69e02667d1f721354eaa
mssisodiya <mssisodiya@bestpeers.com>
2021-02-02 19:17:00 +0530

    added logins action

--- frontend/src/actions/login.js
+++ frontend/src/actions/login.js
@@ -2,11 +2,23 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { apiUrl } from "./../config.json";
 
-export const login = (data) => {
+export const elogin = (data) => {
   return async (dispatch) => {
-    const { data: user } = await axios.post(`${apiUrl}auth/login`, data);
+    const { data: user } = await axios.post(`${apiUrl}auth/elogin`, data);
     dispatch({
-      type: "LOGIN",
+      type: "ELOGIN",
+      payload: user,
+    });
+    toast.success("Logged in succesfully");
+    return user;
+  };
+};
+
+export const jlogin = (data) => {
+  return async (dispatch) => {
+    const { data: user } = await axios.post(`${apiUrl}auth/jlogin`, data);
+    dispatch({
+      type: "JLOGIN",
       payload: user,
     });
     toast.success("Logged in succesfully");
